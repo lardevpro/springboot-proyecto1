c8c2c945bd89bc4123bd2a8ba1c729a9fe805343
lardevpro <lardevpro@gmail.com>
2025-03-14 23:16:25 +0100

    config first commit

---
+++ src/main/java/es/pildoras/proyecto1/Poryecto1/Poryecto1Application.java
@@ -0,0 +1,13 @@
+package es.pildoras.proyecto1.Poryecto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poryecto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poryecto1Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/pildoras/proyecto1/Poryecto1/Poryecto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.pildoras.proyecto1.Poryecto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poryecto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
